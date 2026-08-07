5b2a8b5449db418f144860986df331b71cf05ad6
KIM TAEK KWON <cheng80@nate.com>
2026-08-07 16:33:29 +0900

    first commit

---
+++ script/section01/01-01/01-data.js
@@ -0,0 +1,22 @@
+console.log("----------------------");
+
+const 프로필 = {
+    이름 : "철수",
+    나이 : 30,
+    학교 : "강동캠퍼스"
+};
+
+console.log(프로필);
+
+console.table(프로필);
+
+console.log(프로필.나이);
+
+const 쇼핑리스트 = ["맥북", "에어팟", "아이폰"];
+
+console.log(쇼핑리스트);
+console.log(쇼핑리스트.length);
+
+쇼핑리스트.push("아이패드");
+
+console.log(쇼핑리스트);
