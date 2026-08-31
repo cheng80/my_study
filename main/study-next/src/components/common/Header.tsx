1f2667f5432709d9690238f0f6e8ad1374beb984
KIM TAEK KWON <cheng80@nate.com>
2026-08-31 10:01:15 +0900

    20260828_수업

--- main/study-next/src/components/common/Header.tsx
+++ main/study-next/src/components/common/Header.tsx
@@ -5,13 +5,17 @@ export default function Header() {
   return (
     <header className={styles.header}>
       <Link className={styles.logo} href="/">
-        TRIP STUDY
+        NEXT STUDY
       </Link>
 
       <nav className={styles.menu}>
         <Link href="/">CSS Module</Link>
         <Link href="/tailwind">Tailwind CSS</Link>
         <Link href="/styled-components">styled-components</Link>
+        <Link href="/07-http-async-cors">HTTP Async CORS</Link>
+        <Link className={styles.apolloLink} href="/apollo">
+          Apollo Client
+        </Link>
       </nav>
     </header>
   );
